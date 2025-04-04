acffbc38fe88fb616ddc8b273a3a104c507c6fb6
Epsilon <epsijes@gmail.com>
2025-04-04 14:18:03 +0200

    Update load_data.py
    Use table_order_and_key

--- src/load_data.py
+++ src/load_data.py
@@ -6,6 +6,7 @@ import communicate_db
 import transform_data
 from os.path import join
 import json
+from table_order_and_keys import get_order
 
 
 def load_table(data: pl.DataFrame, name: str, credentials) -> None:
@@ -15,24 +16,16 @@ def load_table(data: pl.DataFrame, name: str, credentials) -> None:
     data.write_database(table_name=name, connection=uri, if_table_exists="append")
 
 
-def main():
+def populate_tables(order: list[str]):
     creds = join("Data", "my_db.json")
     tables = transform_data.main()
-    order = [
-        "customers",
-        "brands",
-        "categories",
-        "stores",
-        "products",
-        "stocks",
-        "staffs",
-        "orders",
-        "order_items",
-    ]
 
     for name in order:
         load_table(tables[name], name, creds)
 
+def main():
+    order = get_order()
+    populate_tables(order)
 
 if __name__ == "__main__":
     main()
