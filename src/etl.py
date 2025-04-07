1bc04e12e984e7faf1bc2b1608f684c2326fe741
Epsilon <epsijes@gmail.com>
2025-04-07 09:36:32 +0200

    Update etl.py
    Pipelineing

--- src/etl.py
+++ src/etl.py
@@ -4,9 +4,20 @@
 # CREDENTIALS will be stored in seperate file,
 #  and should be changed to match your credentials.
 #
+import extract_data
+import transform_data
+import load_data
+import create_db
+import table_order_and_keys
 
 def main():
-    pass
+    order = table_order_and_keys.get_order()
+    tables = {}
+    for table in order:
+        tables[table] = extract_data.extract(table)
+    tables = transform_data.transform_all(tables)
+    create_db.create_my_db(tables)
+    load_data.populate_tables(order, tables)
 
 if __name__ == "__main__":
     main()
