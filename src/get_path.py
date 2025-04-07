304bd15cc58a31b07f3d51121abb548f6c2dcbe4
Epsilon <epsijes@gmail.com>
2025-04-07 12:04:05 +0200

    Update get_path.py
    Get path to local version of tables

--- src/get_path.py
+++ src/get_path.py
@@ -15,6 +15,18 @@ def csv_path(table:str) -> str:
     """
     return join("Data CSV", f"{table}.csv")
 
+def api_path() -> str:
+    """
+    OS specific path to API csv tables
+    """
+    return join("Data API", "data")
+
+def db_path() -> str:
+    """
+    OS specific path to DB csv tables
+    """
+    return join("Data DB")
+
 def main():
     pass
 
