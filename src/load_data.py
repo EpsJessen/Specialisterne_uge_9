3493f92f7d38bebede00046b4d6cc2f1848ac244
Epsilon <epsijes@gmail.com>
2025-04-11 12:30:00 +0200

    Update load_data.py
    Better error handling, move potential error to outer function

--- src/load_data.py
+++ src/load_data.py
@@ -6,32 +6,41 @@ import transform_data
 from get_path import my_creds_path
 import json
 from table_order_and_keys import get_order
+from exceptions import DictNotFoundError
 
 
-def load_table(data: pl.DataFrame, name: str, credentials) -> None:
+def load_table(data: pl.DataFrame, name: str, credentials: dict[str:str]) -> None:
     """Loads a table in db with values from polars dataframe
 
     Args:
         data (pl.DataFrame): The dataframe with the data matching a table in db
         name (str): The name of the target table
-        credentials (_type_): Path to json file containing credentials for using db
+        credentials (dict[str:str]): credentials for using db
     """
-    with open(credentials) as json_credentials:
-        creds: dict = json.load(json_credentials)
-    uri = f"mysql://{creds["user"]}:{creds["passwd"]}@localhost:3306/bikes"
+    uri = f"mysql://{credentials["user"]}:{credentials["passwd"]}@localhost:3306/bikes"
     data.write_database(table_name=name, connection=uri, if_table_exists="append")
 
 
-def load_tables(order: list[str], tables: dict[str, pl.DataFrame]):
+def load_tables(order: list[str], tables: dict[str, pl.DataFrame], creds: str|None = None):
     """Sequentially adds data to tables in db
 
     Args:
         order (list[str]): The order the tables should be loaded
         tables (dict[str, pl.DataFrame]): The dataframes containing the data by name
+        creds (str): Path to json file containing credentials for using db
     """
-    creds = my_creds_path()
-    for name in order:
-        load_table(tables[name], name, creds)
+    if creds is None:
+        creds = my_creds_path()
+    try:
+        with open(creds) as json_credentials:
+            creds: dict = json.load(json_credentials)
+    except FileNotFoundError:
+        raise DictNotFoundError
+    try:
+        for name in order:
+            load_table(tables[name], name, creds)
+    except:
+        raise ConnectionError
 
 
 def main():
